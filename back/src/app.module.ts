5644d1bc6dd8db2e6501ee37f9efb20cfab2b508
httpsJadson <dev.jadson27@gmail.com>
2026-04-07 10:50:39 -0300

    feat: add initialization for users and auth modules

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AssignatureTypeModule } from './assignature-type/assignature-type.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AssignatureTypeModule],
+  imports: [AssignatureTypeModule, UserModule, AuthModule],
   controllers: [],
   providers: [],
 })
